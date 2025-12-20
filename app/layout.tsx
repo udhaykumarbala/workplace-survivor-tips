a18e7fd995ed08a5fda40afbb9b071c2b91b0623
Arun <arungauthamk@gmail.com>
2025-12-20 10:06:30 +0530

    feat: integrate Vercel Analytics for enhanced tracking
    - Added @vercel/analytics dependency to package.json and package-lock.json.
    - Implemented <Analytics /> component in the RootLayout for user interaction tracking.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Analytics } from '@vercel/analytics/next';
+
 
 const inter = Inter({
   variable: "--font-inter",
@@ -41,6 +43,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${inter.variable} font-sans antialiased`}>
         {children}
+        <Analytics />
       </body>
     </html>
   );
